323cecb7ec4f4aadb81b02393d0065936698bdab
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-06-26 18:42:49 -0300

    Update AcademiaDBContext.cs

--- Data/AcademiaDBContext.cs
+++ Data/AcademiaDBContext.cs
@@ -7,5 +7,7 @@ public class AcademiaDBContext : DbContext
 {
     public AcademiaDBContext(DbContextOptions<AcademiaDBContext> options) : base(options) { }
 
-    public DbSet<Aluno> Aluno { get; set; }
+    public DbSet<Aluno> Alunos { get; set; }
+    public DbSet<Equipamento> Equipamentos { get; set; }
+    public DbSet<TipoExercicio> TiposExercicio { get; set; }
 }
