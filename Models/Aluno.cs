530c632d76a49e54d3a842d620eb3aaed3605cfc
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-06-26 14:48:19 -0300

    Projeto criado
    Projeto criado

---
+++ Models/Aluno.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AcademiaFacil.Models
+{
+    public class Aluno
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int MyProperty { get; set; }
+    }
+}
