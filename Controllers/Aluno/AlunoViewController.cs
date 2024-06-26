78d26af3221bccd09fad327a93c8c60ff321a007
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-06-26 14:42:36 -0300

    Projeto criado
    Projeto criado

---
+++ Controllers/Aluno/AlunoViewController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AcademiaFacil.Controllers.Aluno
+{
+    public class AlunoViewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Data/AcademiaDBContext.cs
@@ -0,0 +1,11 @@
+﻿using AcademiaFacil.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AcademiaFacil.Data;
+
+public class AcademiaDBContext : DbContext
+{
+    public AcademiaDBContext(DbContextOptions<AcademiaDBContext> options) : base(options) { }
+
+    public DbSet<Aluno> Aluno { get; set; }
+}
