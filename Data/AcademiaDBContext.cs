3a9f20e53a2ec902164e10adf736c096e8747a5b
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-06-26 21:57:49 -0300

    Criação de mais tabelas
    Criação de mais tabelas

--- Data/AcademiaDBContext.cs
+++ Data/AcademiaDBContext.cs
@@ -1,4 +1,6 @@
 ﻿using AcademiaFacil.Models;
+using AcademiaFacil.Models.Entidades;
+using AcademiaFacil.Models.Tipos;
 using Microsoft.EntityFrameworkCore;
 
 namespace AcademiaFacil.Data;
@@ -8,6 +10,9 @@ public class AcademiaDBContext : DbContext
     public AcademiaDBContext(DbContextOptions<AcademiaDBContext> options) : base(options) { }
 
     public DbSet<Aluno> Alunos { get; set; }
+    public DbSet<Professor> Professores { get; set; }
+    public DbSet<Treino> Treinos { get; set; }
+    public DbSet<Exercicio> Exercicios { get; set; }
     public DbSet<Equipamento> Equipamentos { get; set; }
     public DbSet<TipoExercicio> TiposExercicio { get; set; }
 }
