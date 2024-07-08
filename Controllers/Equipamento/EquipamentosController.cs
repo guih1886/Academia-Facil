76877ba570fac9e361f2401a02e248579fcdffa9
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-07-08 18:30:39 -0300

    Criado controlador de equipamentos
    Criado controlador de equipamentos

---
+++ Controllers/Equipamento/EquipamentosController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AcademiaFacil.Controllers.Equipamentos;
+
+public class EquipamentosController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Cadastro()
+    {
+        return View();
+    }
+
+}
