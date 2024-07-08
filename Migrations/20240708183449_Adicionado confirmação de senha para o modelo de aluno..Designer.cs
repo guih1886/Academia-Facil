6938a195d7a177fad2209cf04b2ce308ff4db179
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-07-08 16:22:48 -0300

    Adicionado confirmação de senha para o modelo de aluno
    Adicionado confirmação de senha para o modelo de aluno

--- Migrations/20240708183449_Adicionado confirmação de senha para o modelo de aluno..Designer.cs
+++ Migrations/20240708183449_Adicionado confirmação de senha para o modelo de aluno..Designer.cs
@@ -4,6 +4,7 @@ using AcademiaFacil.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AcademiaFacil.Migrations
 {
     [DbContext(typeof(AcademiaDBContext))]
-    partial class AcademiaDBContextModelSnapshot : ModelSnapshot
+    [Migration("20240708183449_Adicionado confirmação de senha para o modelo de aluno.")]
+    partial class Adicionadoconfirmaçãodesenhaparaomodelodealuno
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20240708183449_Adicionado confirmação de senha para o modelo de aluno..cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AcademiaFacil.Migrations
+{
+    /// <inheritdoc />
+    public partial class Adicionadoconfirmaçãodesenhaparaomodelodealuno : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ConfirmacaoSenha",
+                table: "Alunos",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConfirmacaoSenha",
+                table: "Alunos");
+        }
+    }
+}
