7c1946ed2b3970ca183d2ef64c3edf4008ba2388
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-07-08 18:31:47 -0300

    Atualizado o modelo de equipamentos
    Atualizado o modelo de equipamentos

--- Models/Equipamento.cs
+++ Models/Equipamento.cs
@@ -6,6 +6,8 @@ namespace AcademiaFacil.Models;
 [Owned]
 public class Equipamento
 {
+    public Equipamento()    {    }
+
     public Equipamento(int id, bool ativo, string imagem, string nome, string descricao, IEnumerable<float> relacaoCargas, string? ajuda)
     {
         Id = id;
@@ -18,24 +20,24 @@ public class Equipamento
     }
 
     [Key]
-    public int Id { get; set; }
+    public int? Id { get; set; }
 
     public bool Ativo { get; set; }
 
     [Required(ErrorMessage = "A imagem do equipamento deve ser informada.")]
     [DataType(DataType.Url, ErrorMessage = "Url inválida.")]
-    public string Imagem { get; set; }
+    public string? Imagem { get; set; }
 
     [Required(ErrorMessage = "O nome do equipamento deve ser informado.")]
     [StringLength(100, MinimumLength = 10, ErrorMessage = "Nome muito curto.")]
-    public string Nome { get; set; }
+    public string? Nome { get; set; }
 
     [Required(ErrorMessage = "A descrição do equipamento deve ser informada.")]
     [StringLength(150, MinimumLength = 20, ErrorMessage = "Descrição muito curta.")]
-    public string Descricao { get; set; }
+    public string? Descricao { get; set; }
 
     [Required(ErrorMessage = "A relação das cargas deve ser informada.")]
-    public IEnumerable<float> RelacaoCargas { get; set; }
+    public IEnumerable<float>? RelacaoCargas { get; set; }
 
     [DataType(DataType.Url, ErrorMessage = "Url inválida.")]
     public string? Ajuda { get; set; }
