050c766435bb2222f63181cc45a97ae2c2d2a296
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-07-13 17:10:12 -0300

    Criado view para erros
    Criado view para erros

---
+++ Models/View/ErrorModel.cs
@@ -0,0 +1,16 @@
+﻿namespace AcademiaFacil.Models.View;
+
+public class ErrorModel
+{
+    public ErrorModel(string statusCode, string mensagem, string imagem)
+    {
+        StatusCode = statusCode;
+        Mensagem = mensagem;
+        Imagem = imagem;
+    }
+
+    public string StatusCode { get; set; }
+    public string Mensagem { get; set; }
+
+    public string Imagem { get; set; }
+}
