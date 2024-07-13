d31a8f0c1492239a1d50ec18aabf746973d3f072
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-07-13 17:34:14 -0300

    Teste

---
+++ Controllers/Exercicio/ExerciciosController.cs
@@ -0,0 +1,18 @@
+﻿using AcademiaFacil.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AcademiaFacil.Controllers.Exercicios;
+
+public class ExerciciosController : Controller
+{
+    public IActionResult Index()
+    {
+        List<Exercicio> lista = new List<Exercicio>();
+        return View(lista);
+    }
+
+    public IActionResult Cadastrar()
+    {
+        return View();
+    }
+}
