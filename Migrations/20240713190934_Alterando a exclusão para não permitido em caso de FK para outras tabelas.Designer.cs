d5309afc12cbb9da1841a64781a500283ee89e5d
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-07-13 17:09:05 -0300

    Atualizado modo de exclusão da tabela
    Atualizado modo de exclusão da tabela

--- Migrations/20240713190934_Alterando a exclusão para não permitido em caso de FK para outras tabelas.Designer.cs
+++ Migrations/20240713190934_Alterando a exclusão para não permitido em caso de FK para outras tabelas.Designer.cs
@@ -4,6 +4,7 @@ using AcademiaFacil.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AcademiaFacil.Migrations
 {
     [DbContext(typeof(AcademiaDBContext))]
-    partial class AcademiaDBContextModelSnapshot : ModelSnapshot
+    [Migration("20240713190934_Alterando a exclusão para não permitido em caso de FK para outras tabelas")]
+    partial class AlterandoaexclusãoparanãopermitidoemcasodeFKparaoutrastabelas
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20240713190934_Alterando a exclusão para não permitido em caso de FK para outras tabelas.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AcademiaFacil.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterandoaexclusãoparanãopermitidoemcasodeFKparaoutrastabelas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Equipamentos_RelacaoCargas_RelacaoCargasId",
+                table: "Equipamentos");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Equipamentos_RelacaoCargas_RelacaoCargasId",
+                table: "Equipamentos",
+                column: "RelacaoCargasId",
+                principalTable: "RelacaoCargas",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Equipamentos_RelacaoCargas_RelacaoCargasId",
+                table: "Equipamentos");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Equipamentos_RelacaoCargas_RelacaoCargasId",
+                table: "Equipamentos",
+                column: "RelacaoCargasId",
+                principalTable: "RelacaoCargas",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
