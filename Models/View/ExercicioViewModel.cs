7bcebda4850990058413d2f59bdf1f7a259841fc
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-07-31 18:58:02 -0300

    Create ExercicioViewModel.cs

---
+++ Models/View/ExercicioViewModel.cs
@@ -0,0 +1,19 @@
+﻿using AcademiaFacil.Models.Tipos;
+
+namespace AcademiaFacil.Models.View;
+
+public class ExercicioViewModel
+{
+    public ExercicioViewModel() { }
+
+    public ExercicioViewModel(Exercicio? exercicio, List<TipoExercicio>? tiposExercicio, List<Equipamento> equipamentos)
+    {
+        Exercicio = exercicio;
+        TiposExercicio = tiposExercicio;
+        Equipamentos = equipamentos;
+    }
+
+    public Exercicio? Exercicio { get; set; }
+    public List<TipoExercicio>? TiposExercicio { get; set; }
+    public List<Equipamento>? Equipamentos { get; set; }
+}
