c2ec80545eefb5c7ffa13cd2923c88ea07ef5b58
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-07-31 18:57:18 -0300

    Criado repository para exercicios
    Criado repository para exercicios

---
+++ Data/Interfaces/Repository/IExercicioRepository.cs
@@ -0,0 +1,9 @@
+﻿using AcademiaFacil.Models;
+
+namespace AcademiaFacil.Data.Interfaces.Repository
+{
+    public interface IExercicioRepository
+    {
+        Exercicio CadastrarExercicio(Exercicio exercicio);
+    }
+}
---
+++ Data/Repositories/ExercicioRepository.cs
@@ -0,0 +1,22 @@
+﻿using AcademiaFacil.Data.Interfaces.Repository;
+using AcademiaFacil.Models;
+
+namespace AcademiaFacil.Data.Repositories
+{
+    public class ExercicioRepository : IExercicioRepository
+    {
+        private AcademiaDBContext _dbContext;
+
+        public ExercicioRepository(AcademiaDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Exercicio CadastrarExercicio(Exercicio exercicio)
+        {
+            _dbContext.Exercicios.Add(exercicio);
+            _dbContext.SaveChanges();
+            return exercicio;
+        }
+    }
+}
