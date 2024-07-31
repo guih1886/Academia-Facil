a836074cbd918c91213c00fce7b2346e3cbbf5c5
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-07-31 18:55:52 -0300

    Adicionado nível para o modelo de tipo de exercicio
    Adicionado nível para o modelo de tipo de exercicio

--- Migrations/20240731194620_Adicionado nível para o modelo de tipo de exercicío.Designer.cs
+++ Migrations/20240731194620_Adicionado nível para o modelo de tipo de exercicío.Designer.cs
@@ -4,6 +4,7 @@ using AcademiaFacil.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AcademiaFacil.Migrations
 {
     [DbContext(typeof(AcademiaDBContext))]
-    partial class AcademiaDBContextModelSnapshot : ModelSnapshot
+    [Migration("20240731194620_Adicionado nível para o modelo de tipo de exercicío")]
+    partial class Adicionadonívelparaomodelodetipodeexercicío
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20240731194620_Adicionado nível para o modelo de tipo de exercicío.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AcademiaFacil.Migrations
+{
+    /// <inheritdoc />
+    public partial class Adicionadonívelparaomodelodetipodeexercicío : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "TiposExercicio",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Nivel",
+                table: "TiposExercicio",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Nivel",
+                table: "TiposExercicio");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "TiposExercicio",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(150)",
+                oldMaxLength: 150);
+        }
+    }
+}
