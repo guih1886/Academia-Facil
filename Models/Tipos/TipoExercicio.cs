49a3a9181950c1b0b55611c7826cefcf8c06403d
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-07-31 18:56:31 -0300

    Adicionado nível para o modelo de tipo de exercicio
    Adicionado nível para o modelo de tipo de exercicio

--- Models/Tipos/TipoExercicio.cs
+++ Models/Tipos/TipoExercicio.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AcademiaFacil.Data.Enums;
+using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
 
@@ -7,12 +8,15 @@ namespace AcademiaFacil.Models.Tipos;
 [Owned]
 public class TipoExercicio
 {
-    public TipoExercicio(int id, bool ativo, string nome, string descricao)
+    public TipoExercicio()   {   }
+
+    public TipoExercicio(int id, bool ativo, string nome, string descricao, Nivel nivel)
     {
         Id = id;
         Ativo = ativo;
         Nome = nome;
         Descricao = descricao;
+        Nivel = nivel;
     }
 
     [Key]
@@ -22,9 +26,12 @@ public class TipoExercicio
 
     [Required(ErrorMessage = "O nome deve ser informado."), NotNull]
     [StringLength(50, MinimumLength = 10, ErrorMessage = "Nome do tipo de exercício incorreto.")]
-    public string Nome { get; set; }
+    public string? Nome { get; set; }
 
     [Required(ErrorMessage = "A descrição deve ser informada."), NotNull]
-    [StringLength(50, MinimumLength = 10, ErrorMessage = "Descrição do tippo do exercício incorreto.")]
-    public string Descricao { get; set; }
+    [StringLength(150, MinimumLength = 10, ErrorMessage = "Descrição do tipo do exercício incorreto.")]
+    public string? Descricao { get; set; }
+
+    [Required(ErrorMessage = "O nível do treino deve ser informada."), NotNull]
+    public Nivel Nivel { get; set; }
 }
