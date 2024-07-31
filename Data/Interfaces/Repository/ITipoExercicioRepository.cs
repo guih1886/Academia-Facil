ca68bb29f9861ffab1d0b91ed1b87162de69e4ef
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-07-31 18:57:28 -0300

    Criado repository para tipo de exercicios
    Criado repository para tipo de exercicios

---
+++ Data/Interfaces/Repository/ITipoExercicioRepository.cs
@@ -0,0 +1,13 @@
+﻿using AcademiaFacil.Models.Tipos;
+
+namespace AcademiaFacil.Data.Interfaces.Repository
+{
+    public interface ITipoExercicioRepository
+    {
+        TipoExercicio CadastrarTipoExercicio(TipoExercicio tipoExercicio);
+        TipoExercicio AtualizarTipoExercicio(TipoExercicio tipoExercicio);
+        TipoExercicio? BuscarTipoExercicioPorId(int id);
+        List<TipoExercicio> ListaTiposExercicio();
+        bool DeletarTipoExercicio(int id);
+    }
+}
---
+++ Data/Repositories/TiposExercicioRepository.cs
@@ -0,0 +1,53 @@
+﻿using AcademiaFacil.Data.Interfaces.Repository;
+using AcademiaFacil.Models.Tipos;
+
+namespace AcademiaFacil.Data.Repositories
+{
+    public class TiposExercicioRepository : ITipoExercicioRepository
+    {
+        private AcademiaDBContext _dbContext;
+
+        public TiposExercicioRepository(AcademiaDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public TipoExercicio AtualizarTipoExercicio(TipoExercicio tipoExercicio)
+        {
+            _dbContext.TiposExercicio.Update(tipoExercicio);
+            _dbContext.SaveChanges();
+            return tipoExercicio;
+        }
+
+        public TipoExercicio? BuscarTipoExercicioPorId(int id)
+        {
+            TipoExercicio? tipoExercicio = _dbContext.TiposExercicio.FirstOrDefault(te => te.Id == id);
+            return tipoExercicio;
+        }
+
+        public TipoExercicio CadastrarTipoExercicio(TipoExercicio tipoExercicio)
+        {
+            tipoExercicio.Ativo = true;
+            _dbContext.TiposExercicio.Add(tipoExercicio);
+            _dbContext.SaveChanges();
+            return tipoExercicio;
+        }
+
+        public bool DeletarTipoExercicio(int id)
+        {
+            TipoExercicio? tipoExercicio = BuscarTipoExercicioPorId(id);
+            if (tipoExercicio != null)
+            {
+                _dbContext.TiposExercicio.Remove(tipoExercicio);
+                _dbContext.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+
+        public List<TipoExercicio> ListaTiposExercicio()
+        {
+            return _dbContext.TiposExercicio.ToList();
+        }
+    }
+}
