4b9ced133e9164aa6aaf6f258ac735859e1bbf3b
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-08-02 16:25:18 -0300

    Criação do repository de exercicios
    Criação do repository de exercicios

--- Data/Interfaces/Repository/IExercicioRepository.cs
+++ Data/Interfaces/Repository/IExercicioRepository.cs
@@ -4,6 +4,10 @@ namespace AcademiaFacil.Data.Interfaces.Repository
 {
     public interface IExercicioRepository
     {
+        Exercicio AtualizarExercicio(Exercicio exercicio);
         Exercicio CadastrarExercicio(Exercicio exercicio);
+        bool DeletarExercicio(int id);
+        List<Exercicio> GetExercicios();
+        Exercicio? GetExerciciosPorId(int id);
     }
 }
--- Data/Repositories/ExercicioRepository.cs
+++ Data/Repositories/ExercicioRepository.cs
@@ -12,11 +12,44 @@ namespace AcademiaFacil.Data.Repositories
             _dbContext = dbContext;
         }
 
+        public Exercicio AtualizarExercicio(Exercicio exercicio)
+        {
+            _dbContext.Exercicios.Update(exercicio);
+            _dbContext.SaveChanges();
+            return exercicio;
+        }
+
         public Exercicio CadastrarExercicio(Exercicio exercicio)
         {
+            exercicio.Ativo = true;
             _dbContext.Exercicios.Add(exercicio);
             _dbContext.SaveChanges();
             return exercicio;
+        }
+
+        public bool DeletarExercicio(int id)
+        {
+            Exercicio exercicio = GetExerciciosPorId(id);
+            try
+            {
+                _dbContext.Exercicios.Remove(exercicio);
+                _dbContext.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public List<Exercicio> GetExercicios()
+        {
+            return _dbContext.Exercicios.ToList();
+        }
+
+        public Exercicio? GetExerciciosPorId(int id)
+        {
+            return _dbContext.Exercicios.FirstOrDefault(ex => ex.Id == id);
         }
     }
 }
