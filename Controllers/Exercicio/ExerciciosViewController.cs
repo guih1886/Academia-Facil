c01899cadf70ccd25ddc4e8af1ab97f91da967ac
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-08-02 16:26:36 -0300

    Criação da edição de exercicios
    Criação da edição de exercicios

--- Controllers/Exercicio/ExerciciosViewController.cs
+++ Controllers/Exercicio/ExerciciosViewController.cs
@@ -1,6 +1,8 @@
-﻿using AcademiaFacil.Data.Interfaces.Repository;
+﻿using AcademiaFacil.Configuration;
+using AcademiaFacil.Data.Interfaces.Repository;
 using AcademiaFacil.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace AcademiaFacil.Controllers.Exercicios;
 
@@ -9,17 +11,23 @@ public class ExerciciosViewController : Controller
     private IEquipamentoRepository _equipamentoRepository;
     private ITipoExercicioRepository _tipoExercicioRepository;
     private IExercicioRepository _exercicioRepository;
+    private IRelacaoCargasRepository _relacaoCargasRepository;
+    private ConfigPadroes configuracao;
 
-    public ExerciciosViewController(IEquipamentoRepository equipamentoRepository, ITipoExercicioRepository tipoExercicioRepository, IExercicioRepository exercicioRepository)
+    public ExerciciosViewController(IEquipamentoRepository equipamentoRepository, ITipoExercicioRepository tipoExercicioRepository, IExercicioRepository exercicioRepository, IOptions<ConfigPadroes> config, IRelacaoCargasRepository relacaoCargasRepository)
     {
         _equipamentoRepository = equipamentoRepository;
         _tipoExercicioRepository = tipoExercicioRepository;
         _exercicioRepository = exercicioRepository;
+        configuracao = new ConfigPadroes();
+        configuracao.NumeroRepeticoesPadrao = config.Value.NumeroRepeticoesPadrao;
+        _relacaoCargasRepository = relacaoCargasRepository;
     }
 
     public IActionResult Index()
     {
-        List<Exercicio> lista = new List<Exercicio>();
+        List<Exercicio> lista = _exercicioRepository.GetExercicios();
+        PreencheViewBag();
         return View(lista);
     }
 
@@ -27,6 +35,37 @@ public class ExerciciosViewController : Controller
     {
         PreencheViewBag();
         return View();
+    }
+
+    public IActionResult Editar(int id)
+    {
+        Exercicio? exercicio = _exercicioRepository.GetExerciciosPorId(id);
+        PreencheViewBag();
+        return View(exercicio);
+    }
+
+    public IActionResult EditarExercicio(Exercicio exercicio)
+    {
+        if (ModelState.IsValid)
+        {
+            _exercicioRepository.AtualizarExercicio(exercicio);
+            return RedirectToAction("Index");
+        }
+        else
+        {
+            PreencheViewBag();
+            return View("Editar", exercicio);
+        }
+    }
+
+    public IActionResult Deletar(int id)
+    {
+        if (_exercicioRepository.DeletarExercicio(id))
+        {
+            return RedirectToAction("Index");
+        }
+        //TODO redirecionar para a página de erro
+        return RedirectToAction("//");
     }
 
     [HttpPost]
@@ -42,13 +81,14 @@ public class ExerciciosViewController : Controller
             PreencheViewBag();
             return View("Cadastrar", exercicio);
         }
-
     }
 
     private dynamic PreencheViewBag()
     {
         ViewBag.TiposExercicios = _tipoExercicioRepository.ListaTiposExercicio().Where(te => te.Ativo == true).ToList();
         ViewBag.Equipamentos = _equipamentoRepository.GetEquipamentos().Where(equip => equip.Ativo == true).ToList();
+        ViewBag.RelacaoCargas = _relacaoCargasRepository.GetRelacaoCargas()!.Where(rc => rc.Ativo == true).ToList();
+        ViewBag.RepeticaoPadrao = configuracao.NumeroRepeticoesPadrao;
         return ViewBag;
     }
 }
