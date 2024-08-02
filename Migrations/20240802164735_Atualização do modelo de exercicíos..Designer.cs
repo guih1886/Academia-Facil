0d63127571dd5fa8ca599101505d5c2fd0dfa609
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-08-02 16:23:22 -0300

    Atualização do modelo de exercicíos
    Atualização do modelo de exercicíos

--- Migrations/20240802164735_Atualização do modelo de exercicíos..Designer.cs
+++ Migrations/20240802164735_Atualização do modelo de exercicíos..Designer.cs
@@ -4,6 +4,7 @@ using AcademiaFacil.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AcademiaFacil.Migrations
 {
     [DbContext(typeof(AcademiaDBContext))]
-    partial class AcademiaDBContextModelSnapshot : ModelSnapshot
+    [Migration("20240802164735_Atualização do modelo de exercicíos.")]
+    partial class Atualizaçãodomodelodeexercicíos
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/Exercicio.cs
+++ Models/Exercicio.cs
@@ -1,10 +1,8 @@
 ﻿using AcademiaFacil.Data.Enums;
 using AcademiaFacil.Models.Tipos;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
-using System.Configuration;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics.CodeAnalysis;
 
 namespace AcademiaFacil.Models;
@@ -14,35 +12,33 @@ public class Exercicio
 {
     public Exercicio() { }
 
-    public Exercicio(int id, bool ativo, string nome, string descricao, TipoExercicio tipoExercicio, int repeticoes, float carga, string? ajuda, string? observacao, Equipamento equipamento)
+    public Exercicio(int id, bool ativo, string nome, string descricao, int tipoExercicioId, int repeticoes, float carga, string? ajuda, string? observacao, int equipamentoId)
     {
         Id = id;
         Ativo = ativo;
         Nome = nome;
         Descricao = descricao;
-        TipoExercicio = tipoExercicio;
         Repeticoes = repeticoes;
         Carga = carga;
         Ajuda = ajuda;
         Observacao = observacao;
-        Equipamento = equipamento;
+        TipoExercicioId = tipoExercicioId;
+        EquipamentoId = equipamentoId;
     }
 
     [Key]
     public int? Id { get; set; }
 
-    public bool? Ativo { get; set; }
+    public bool Ativo { get; set; }
 
     [Required(ErrorMessage = "O nome do exercício deve ser informado."), NotNull]
-    [StringLength(50, MinimumLength = 10, ErrorMessage = "Nome do exercício incorreto.")]
+    [StringLength(50, MinimumLength = 5, ErrorMessage = "Nome do exercício incorreto.")]
     public string? Nome { get; set; }
 
     [Required(ErrorMessage = "A descrição do exercício deve ser informada."), NotNull]
     [StringLength(100, MinimumLength = 10, ErrorMessage = "Descrição do exercício incorreta.")]
     public string? Descricao { get; set; }
 
-    [Required(ErrorMessage = "O tipo do exercício deve ser inforado."), NotNull]
-    public TipoExercicio? TipoExercicio { get; set; }
 
     [Required(ErrorMessage = "O número de repetições deve ser informado."), NotNull]
     public int Repeticoes { get; set; }
@@ -58,6 +54,16 @@ public class Exercicio
 
     public string? Observacao { get; set; }
 
+    [ForeignKey("Equipamento")]
     [Required(ErrorMessage = "O equipamento deve ser informado.")]
-    public Equipamento? Equipamento { get; set; }
+    public int? EquipamentoId { get; set; }
+
+    public virtual Equipamento? Equipamento { get; set; }
+
+    [ForeignKey("TipoExercicio")]
+    [Required(ErrorMessage = "O tipo do exercício deve ser inforado."), NotNull]
+    public int? TipoExercicioId { get; set; }
+
+    public virtual TipoExercicio? TipoExercicio { get; set; }
+
 }
