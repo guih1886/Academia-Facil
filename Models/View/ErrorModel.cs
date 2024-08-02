e6fbd481fb31f569ae50de81e81822a96341f4eb
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-08-02 16:22:51 -0300

    Atualização da view de erros
    Atualização da view de erros

--- Models/View/ErrorModel.cs
+++ Models/View/ErrorModel.cs
@@ -2,15 +2,24 @@
 
 public class ErrorModel
 {
-    public ErrorModel(string statusCode, string mensagem, string imagem)
+    public ErrorModel(int statusCode, string mensagem, string imagem, string controladorDoErro, string textoBotaoControladorDoErrro, string acaoErro, string controladorVoltar, string acaoVoltar)
     {
         StatusCode = statusCode;
         Mensagem = mensagem;
         Imagem = imagem;
+        ControladorDoErro = controladorDoErro;
+        TextoBotaoControladorDoErrro = textoBotaoControladorDoErrro;
+        AcaoErro = acaoErro;
+        ControladorVoltar = controladorVoltar;
+        AcaoVoltar = acaoVoltar;
     }
 
-    public string StatusCode { get; set; }
+    public int StatusCode { get; set; }
     public string Mensagem { get; set; }
-
     public string Imagem { get; set; }
+    public string ControladorDoErro { get; set; }
+    public string TextoBotaoControladorDoErrro { get; set; }
+    public string AcaoErro { get; set; }
+    public string ControladorVoltar { get; set; }
+    public string AcaoVoltar { get; set; }
 }
